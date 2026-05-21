32e00b7c32a27ac348c7ed49885843e32b935cb8
ankapena63 <ankapena@gmail.com>
2026-05-21 20:56:28 +0200

    Update user_config_override.h for SML and scripts

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -1,14 +1,14 @@
 #ifndef USER_CONFIG_OVERRIDE_H
 #define USER_CONFIG_OVERRIDE_H
 
-#undef USE_SML        // ❗ GANZ WICHTIG (schaltet alte SML AUS)
+#define USE_SCRIPT      // ✅ notwendig für xsns_53
+#define USE_SML         // ✅ alter SML (passt zu deinem Code)
 
-#define USE_SML_M     // neue SML
-#define USE_BERRY
-#define USE_UFILESYS
+#define USE_BERRY       // optional
 #define USE_ETHERNET
 #define USE_MQTT
 
 #define FRIENDLY_NAME "SML-WT32"
 
 #endif
+``
