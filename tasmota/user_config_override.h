cf38aeb6d1feff40ffc3b4cb9598a7f8ed7f7dac
ankapena63 <ankapena@gmail.com>
2026-05-21 19:01:02 +0200

    Add user configuration overrides for WiFi and device

---
+++ tasmota/user_config_override.h
@@ -0,0 +1,12 @@
+#ifndef USER_CONFIG_OVERRIDE_H
+#define USER_CONFIG_OVERRIDE_H
+
+#define WIFI_SSID "DEIN_WLAN"
+#define WIFI_PASS "DEIN_PASS"
+
+#define FRIENDLY_NAME "smlmitlan"
+
+#define USE_SML_M
+
+#endif
+``
